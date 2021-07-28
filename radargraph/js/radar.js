f41387484c96636071e63b2355ef794fbace3795
Aaron Honson <aaron.shannon-honson@adelaide.edu.au>
2021-07-28 10:33:19 +0930

    Updates from Jo - added different feedback for each role

--- radargraph/js/radar.js
+++ radargraph/js/radar.js
@@ -1,10 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     $(document).foundation({
-        reveal : {
-            animation : 'fade'
+        reveal: {
+            animation: 'fade'
         }
     });
+
+    var role;
+    //save the role
+    $("#role").change(function () {
+        role = $(this).val();
+        console.log(role);
+    })
+
+
 
 
     //test out making this re-usable. Use URL parameters to name the json file we're looking for.
@@ -13,10 +22,10 @@ $(document).ready(function() {
             sURLVariables = sPageURL.split('&'),
             sParameterName,
             i;
-    
+
         for (i = 0; i < sURLVariables.length; i++) {
             sParameterName = sURLVariables[i].split('=');
-    
+
             if (sParameterName[0] === sParam) {
                 return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
             }
@@ -25,209 +34,215 @@ $(document).ready(function() {
     };
 
     var choice = getUrlParameter('d');
-    console.log(choice);
+
 
     // variable for saving what the final page is - used to trigger unique functions on the last page click
     var finalPage;
 
-   // read in data, populate the page(s) from a .JSON file (or equivalent)
-    var json = $.getJSON("./data/" + choice + ".json", function(data){
+    // read in data, populate the page(s) from a .JSON file (or equivalent)
+    var json = $.getJSON("./data/" + choice + ".json", function (data) {
 
         var title = data[0].title;
         var intro = data[0].introduction;
 
         $('#intro').append("<h1>" + title + "</h1>");
         $('#intro').append("<p>" + intro + "</p>");
-         //look for entry that dictates if the activity should have a name entry
-            //if so, append it after the intro
+        //look for entry that dictates if the activity should have a name entry
+        //if so, append it after the intro
 
-        for (var i = 0; i < data[0].pages.length; i++){
+        for (var i = 0; i < data[0].pages.length; i++) {
             var id = data[0].pages[i].name.toLowerCase();
             id = id.replace(/\s/g, '');
 
             //open div
-            if(i == 0){
+            if (i == 0) {
                 $('#chart').before(
                     "<div id='" + id + "' class = 'page panel active'>"
 
 
                 );
-             } else {
+            } else {
                 $('#chart').before(
                     "<div id='" + id + "' class = 'page panel'>"
                 );
-             }
+            }
 
 
-             $('#' + id).append("<h2>" + data[0].pages[i].name + "</h2><p>" + data[0].pages[i].description + "</p>" +"<ol class='questions' id='" + id + "-qs'></ol>");
+            $('#' + id).append("<h2>" + data[0].pages[i].name + "</h2><p>" + data[0].pages[i].description + "</p>" + "<ol class='questions' id='" + id + "-qs'></ol>");
 
-                 //loop through questions now
+            //loop through questions now
 
-                 for (var j = 0; j < data[0].pages[i].statements.length; j++){ 
-                     $('#' + id + "-qs").append("<li><label for ='" + id + "-" + j +"'>" + data[0].pages[i].statements[j] + "</label><div class='answer' id='" + id + "-" + j + "'></div>");
-                 }
+            for (var j = 0; j < data[0].pages[i].statements.length; j++) {
+                $('#' + id + "-qs").append("<li><label for ='" + id + "-" + j + "'>" + data[0].pages[i].statements[j] + "</label><div class='answer' id='" + id + "-" + j + "'></div>");
+            }
 
-                if(i == 0){
-                 $('#' + id).append("<div class='row nav'><div class='progress radius round'><span class='meter'></span></div><a href='#' class='next' title='next'><i class='fa-chevron-circle-right fa'></i></a></div>");
-                } else {
-                    $('#' + id).append("<div class='row nav'><div class='progress radius round'><span class='meter'></span></div><a href='#' class='prev' title='back'><i class='fa-chevron-circle-left fa'></i></a><a href='#' class='next' title='next'><i class='fa-chevron-circle-right fa'></i></a></div>");
-                }
+            if (i == 0) {
+                $('#' + id).append("<div class='row nav'><div class='progress radius round'><span class='meter'></span></div><a href='#' class='next' title='next'><i class='fa-chevron-circle-right fa'></i></a></div>");
+            } else {
+                $('#' + id).append("<div class='row nav'><div class='progress radius round'><span class='meter'></span></div><a href='#' class='prev' title='back'><i class='fa-chevron-circle-left fa'></i></a><a href='#' class='next' title='next'><i class='fa-chevron-circle-right fa'></i></a></div>");
+            }
 
-                $('#'+ id + ' .meter').css("width", i / 5 * 100 + '%');
-                 $('#chart').before("</div>");
-             //close div
+            $('#' + id + ' .meter').css("width", i / 5 * 100 + '%');
+            $('#chart').before("</div>");
+            //close div
 
 
-    //set up final page
-    if(i == data[0].pages.length - 1) {
-        finalPage = id;
+            //set up final page
+            if (i == data[0].pages.length - 1) {
+                finalPage = id;
+            }
         }
-    }
 
 
-    //meter styles
-
-   
-
-// append to id rows:
-// <div class='row detail'><h3>title</h3><p>feedback</p></div>
+        //meter styles
 
 
-    // set up listeners
-        $('.next, .prev').on('click', function() {
+
+        // append to id rows:
+        // <div class='row detail'><h3>title</h3><p>feedback</p></div>
+
+
+        // set up listeners
+        $('.next, .prev').on('click', function () {
             var $this = $(this);
-            console.log($this);
+
             var $pages = $('.page');
-            console.log($pages);
-            console.log($this.closest('.page'));
+
+
             var pageNum = $this.closest('.page').index();
-            console.log(pageNum);
+
             $pages.removeClass('active');
-    
+
             if ($this.hasClass('prev')) {
                 pageNum--;
             } else {
                 pageNum++;
             }
 
-            console.log(pageNum);
-    
+
+
             $($pages.get(pageNum)).addClass('active');
         });
 
-           //final page functions
-    $('#' + finalPage + ' .next').on('click', function() {
-        console.log("Final page!");
-        var course = $('.courseName').val();
-        console.log(course);
-       $('#courseTitle').text(course);
-        var labels = [];
-        var scores = [];
-        $('.page').each(function(idx, page) {
+        //final page functions
+        $('#' + finalPage + ' .next').on('click', function () {
 
-            var $page = $(page);
-            var label = $page.find('h2').text();
+            var course = $('.courseName').val();
 
-            // split multiword labels with newlines every other word
-            label = label.replace(/([^\s]*)\s([^\s]*)\s/, "$1 $2\n");
+            $('#courseTitle').text(course);
+            var labels = [];
+            var scores = [];
+            $('.page').each(function (idx, page) {
 
-            var score = 0;
-            var hasAnswers = false;
-            var answerCount = $(page).find('.answer').length;
-            var adjustedCount = answerCount * 20;
-            
-            $(page).find('.answer').each(function(idx, answer) {
-                hasAnswers = true;
-                score += parseFloat($(answer).val());
-               
+                var $page = $(page);
+                var label = $page.find('h2').text();
+
+                // split multiword labels with newlines every other word
+                label = label.replace(/([^\s]*)\s([^\s]*)\s/, "$1 $2\n");
+
+                var score = 0;
+                var hasAnswers = false;
+                var answerCount = $(page).find('.answer').length;
+                var adjustedCount = answerCount * 20;
+
+                $(page).find('.answer').each(function (idx, answer) {
+                    hasAnswers = true;
+                    score += parseFloat($(answer).val());
+
+                });
+
+                if (hasAnswers) {
+
+                    let percent = (score / adjustedCount) * 100;
+                    scores.push(percent);
+                    labels.push(label);
+
+                }
             });
 
-            if (hasAnswers) {
-                console.log("Raw Score: ", score);
-                console.log("Total points for this page: ", adjustedCount)
-                let percent = (score / adjustedCount) * 100;
-                console.log("Percent:", percent);
-                scores.push(percent);
-                labels.push(label);
-                console.log(scores);
-                console.log(labels);
+            showChart('canvas', labels, scores);
+            //set dimension for showFeedback to be one of the roles selected
+
+            var dimensionRole;
+            dimensionRole = '.' + role;
+
+            showFeedback('.feedback ' + dimensionRole + '', '.feedback', scores);
+
+
+            //function to export report pdf
+            var makeReport = function () {
+                $(".page").addClass("active")
+                $(".nav").css("display", "none");
+                $(".butts").css("display", "none");
+                $("#footer").css("display", "none");
+                var element = document.body;
+                html2pdf().set({
+                    pagebreak: {
+                        mode: 'avoid-all',
+                        after: ['.page', '.chart']
+                    }
+                }).from(element).save();
+
+                // $( ".page" ).removeClass("active");
+                $("#chart").addClass("active");
+
             }
+
+            $("#reportThis").on("click", makeReport);
         });
-
-        showChart('canvas', labels, scores);
-        showFeedback('.feedback .detail', '.feedback', scores);
-
-    
-         //function to export report pdf
-        var makeReport = function(){
-            $( ".page" ).addClass("active")
-            $(".nav").css("display","none");
-            $(".butts").css("display","none");
-            $("#footer").css("display","none");
-            var element = document.body;
-            html2pdf().set({
-                pagebreak: { mode: 'avoid-all', after: ['.page', '.chart'] }
-              }).from(element).save();
-
-            // $( ".page" ).removeClass("active");
-            $("#chart").addClass("active");
-
-        }
-    
-        console.log($("#reportThis"));
-        $("#reportThis").on("click",makeReport);
-    });
 
 
         // set up sliders
         var min = 4;
-    var max = 20;
-    var slider_labels = [
-        'Never',
-        'Rarely',
-        'Sometimes',
-        'Often',
-        'Always',
-    ];
-    var density = slider_labels.length;
+        var max = 20;
+        var slider_labels = [
+            'Never',
+            'Rarely',
+            'Sometimes',
+            'Often',
+            'Always',
+        ];
+        var density = slider_labels.length;
 
-    $('.answer').each(function(idx, slider) {
-        $(slider).noUiSliderA11y({
-            'step': 4,
-            'animate': true,
-            'start': min,
-            'range': {
-                'min': min,
-                'max': max
-            }
+        $('.answer').each(function (idx, slider) {
+            $(slider).noUiSliderA11y({
+                'step': 4,
+                'animate': true,
+                'start': min,
+                'range': {
+                    'min': min,
+                    'max': max
+                }
+            });
+            $(slider).noUiSlider_pips({
+                'mode': 'steps',
+                'density': density,
+                'format': {
+                    to: function (value) {
+                        var numSteps = (max - min) / (density - 1);
+                        var idx = (value / numSteps) - 1;
+
+                        if (idx in slider_labels) {
+                            return slider_labels[idx];
+                        }
+                        return '';
+                    }
+                },
+                'filter': function (value) {
+                    // labeled steps are large
+                    if (value % 4 == 0) {
+                        return 1;
+                    }
+                    // remaining steps are small
+                    else {
+                        return 0;
+                    }
+                }
+            });
         });
-        $(slider).noUiSlider_pips({
-            'mode': 'steps',
-            'density': density,
-            'format': {to: function(value) {
-                var numSteps = (max - min) / (density - 1);
-                var idx = (value / numSteps) - 1;
 
-                if (idx in slider_labels) {
-                    return slider_labels[idx];
-                }
-                return '';
-            }},
-            'filter': function(value) {
-                // labeled steps are large
-                if (value % 4 == 0) {
-                    return 1;
-                }
-                // remaining steps are small
-                else {
-                    return 0;
-                }
-            }
-        });
-    });
 
- 
-             
+
 
 
     });
@@ -236,6 +251,7 @@ $(document).ready(function() {
 
     // Draw the chart based on the current scores
     var radarChart = null;
+
     function showChart(canvas, labels, scores) {
 
         var data = {
@@ -255,9 +271,9 @@ $(document).ready(function() {
                 // },
                 {
                     'label': "Your results",
-                    'fillColor':   "rgba(212, 0, 0, 0.2)",
+                    'fillColor': "rgba(212, 0, 0, 0.2)",
                     'strokeColor': "rgba(212, 0, 0, 1)",
-                    'pointColor':  "rgba(212, 0, 0, 1)",
+                    'pointColor': "rgba(212, 0, 0, 1)",
                     'pointStrokeColor': "#fff",
                     'pointHighlightFill': "#fff",
                     'pointHighlightStroke': "rgba(151,187,205,1)",
@@ -268,14 +284,14 @@ $(document).ready(function() {
 
         var config = {
             'scaleShowLabels': false,
-            'scaleLabel' : "<%= value %>%",
+            'scaleLabel': "<%= value %>%",
             'scaleFontSize': 10,
             'scaleOverride': true,
             'scaleSteps': 5,
             'scaleStepWidth': 20,
             'scaleStartValue': 0,
             'multiTooltipTemplate': "<%= value %>%",
-            'legendTemplate' : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"border-color:<%=datasets[i].strokeColor%>;background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+            'legendTemplate': "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"border-color:<%=datasets[i].strokeColor%>;background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
             'pointLabelFontSize': 16,
             'pointLabelLineHeight': 18,
             'pointLabelDelimiter': "\n"
@@ -296,25 +312,44 @@ $(document).ready(function() {
         var $dimensions = $(dimensions);
         var $feedback = $(feedback);
 
-        // console.log($dimensions);
 
         // $(feedback).addClass('hide');
         // $feedback.addClass('hide');
         // $dimensions.addClass('hide');
 
-        $(scores).each(function(idx, score) {
-            if (score < threshold) {
+        $(scores).each(function (idx, score) {
+            //custom extra step for score above threshold but only for CC
+
+            if (role == 'coordinator') {
+                console.log(score + " for " + idx);
+                if (score < 40) {
+                    //show low
+                    var CCdimensions = $('.feedback .ccLow');
+                    $(CCdimensions.get(idx)).removeClass('hide');
+
+                } else if (score > 70) {
+                    //show high
+                    var CCdimensions = $('.feedback .ccHigh');
+                    $(CCdimensions.get(idx)).removeClass('hide');
+
+                } else {
+                    //do nothing
+                }
+            }
+            //standard for all other roles
+            else if (score < threshold) {
                 $($dimensions.get(idx)).removeClass('hide');
                 $feedback.removeClass('hide');
             }
+
         });
     }
 
 
-    
 
 
-   
 
-    
+
+
+
 });
