1a8cc81433002ac50463ce391ea953a1b47dcde3
MichaelBrockhouse <71360179+MichaelBrockhouse@users.noreply.github.com>
2021-09-14 17:15:19 +0930

    Added basic eventListner for Clicking

--- Anatomy Colouring Tool/js/anatomy_code.js
+++ Anatomy Colouring Tool/js/anatomy_code.js
@@ -19,7 +19,8 @@ var parts =[ //array of the boney bois
 
 
 //array of all your section IDs
-var arraySectionIDs = ["section_1", "section_2", "section_3", "section_4", "section_5", "section_6", "section_7", "section_8", "section_9", "section_10", "section_11", "section_12", "section_13", "section_14", "section_15", "section_16", "section_17", "section_18", "section_19", "section_20", "section_21", "section_22", "section_23", "section_24", "section_25"]; 
+var arraySectionIDs = ["section_1", "section_2", "section_3", "section_4", "section_5", "section_6", "section_7", "section_8", "section_9", "section_10", "section_11", "section_12", "section_13", "section_14", "section_15", "section_16", "section_17", "section_18", "section_19", "section_20", "section_21", "section_22", "section_23", "section_24", "section_25", "section_26"]; 
+
 console.log(arraySectionIDs);
 
 console.log(document.getElementById("section_1"));
@@ -28,10 +29,20 @@ console.log(document.getElementById("section_1"));
 for (let i = 0; i < arraySectionIDs.length; i++) { 
     console.log(arraySectionIDs[i]);
     var element = document.getElementById(arraySectionIDs[i]);
-    console.log(element);
     element.classList.add("invisibleClass");
-   
+    
+    element.addEventListener('click', function()  {
+        BollocksTest(i);
+    }
+    );
+
+
     // also set up an eventListener of type click
+}
+
+
+function BollocksTest(e) {
+    console.log(arraySectionIDs[e]);
 }
 
 
