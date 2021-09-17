89c544b09e3897bde38bcf2282a9db3b3c1dff35
MichaelBrockhouse <71360179+MichaelBrockhouse@users.noreply.github.com>
2021-09-17 17:04:21 +0930

    Some minor code changes and some major commenting

--- colouring/js/anatomy_code.js
+++ colouring/js/anatomy_code.js
@@ -1,8 +1,7 @@
-/* Comments */
+//-------Setting Up Variables-----------
 
-
-
-var parts = [ //array of the boney bois
+// An array containing a list of objects which each contain the number of the bone, the name of the bone, it's correct section and it's highlight color
+var arrayBoneParts = [
     {
         "bone_number": 1,
         "bone": "fovea for ligament of head",
@@ -125,119 +124,97 @@ var parts = [ //array of the boney bois
     }
 ];
 
-
-//array of all your section IDs
+// Array of all section_IDs
 var arraySectionIDs = ["section_1", "section_2", "section_3", "section_4", "section_5", "section_6", "section_7", "section_8", "section_9", "section_10", "section_11", "section_12", "section_13", "section_14", "section_15", "section_16", "section_17", "section_18", "section_19", "section_20", "section_21", "section_22", "section_23", "section_24", "section_25", "section_26"];
 
+var boolIsColoring = false; // If the user has selected a colour, this variable is True. It will only be False before anyone has selected a colour, and after the 'Check' button has been clicked.
+var RGBcurrentColor; // A variable to store the RGB value of the currently selected bone's colour.
 
-var currentSelectedBone;
-var colouring = false;
-var currentColor;
+//-------Setting Up the Initial Page-----------
 
-for (i = 0; i < parts.length; i++) {
+// Sets up all the selectable bone options as clickable objects. This is an example of the HTML link that is generated: <a href="#!" onClick="funcSelectBone(this, '1', 'rgb(255,255,255)')">boneName</a>
+for (i = 0; i < arrayBoneParts.length; i++) {
     var element = document.getElementById("boneList");
-    element.innerHTML += "<p><a href='#!' onClick=\"select(this, '" + parts[i].bone_number + "', '" + parts[i].color + "')\">" + parts[i].bone + "</a></p>";
+    element.innerHTML += "<p><a href='#!' onClick=\"funcSelectBone(this, '" + arrayBoneParts[i].color + "')\">" + arrayBoneParts[i].bone + "</a></p>";
 }
 
-// write a function select() which enables colouring and does something
-
-function select(element, bone_number, color) {
-    currentColor = color;
-    // set currentSelectedBone to bone_number
-    currentSelectedBone = bone_number;
-    // set the background of the element to color
-    element.style.backgroundColor = color;
-    // set colouring to true
-    colouring = true;
-
-    // for each item in array, add the class that makes it transparent
-    for (var i = 0; i < arraySectionIDs.length; i++) {
-        var element = document.getElementById(arraySectionIDs[i]);
-        element.classList.add("invisibleClass");
-        if (colouring && !element.classList.contains("selectedClass") && !element.classList.contains("correctClass")) {
-            element.style = "fill: " + currentColor;
-        }
-
-    }
-
-
-
-}
-
-
-
-// for each item in array, add the class that makes it transparent
+// For each object in arraySectionIDs array, add a class that makes it transparent.
 for (let i = 0; i < arraySectionIDs.length; i++) {
     var element = document.getElementById(arraySectionIDs[i]);
     element.classList.add("invisibleClass");
 
-
-    // on click, run the function on the selected item
-    element.addEventListener('click', function (e) {
-        if (colouring && !e.target.parentElement.classList.contains("correctClass")) {
-            setClicks(e, i);
+    // We also set up an EventListener  on click, run the function on the selected item
+    element.addEventListener('click', function (clickEventData) {
+        if (boolIsColoring && !clickEventData.target.parentElement.classList.contains("correctClass")) {
+            funcRevealFill(i);
         }
     });
 }
 
-// function to prepare the sections for interactivity
-function setClicks(e, index) {
+// Prepares the Check Button
+document.getElementById("checker").addEventListener("click", funcCheckProgress);
+
+//-------Functions-----------
+
+// Function to write a function funcSelectBone() which enables colouring and does something
+function funcSelectBone(element, color) {
+    RGBcurrentColor = color;
+    // Set the background of the bone selection option to it's assigned colour
+    element.style.backgroundColor = color;
+    // set colouring to True, because we have now selected a bone option
+    boolIsColoring = true;
+
+    // Because of complexities with the MouseOver event, we do something different... Everytime we select a bone option, we set ALL bone part sections on the image to that colour. However, since they are all invisible, the only one that appears is the one that's being hovered over; thus seeming to change only that colour when you hover over it!
+    for (var i = 0; i < arraySectionIDs.length; i++) {
+        var element = document.getElementById(arraySectionIDs[i]);
+        if (boolIsColoring && !element.classList.contains("selectedClass") && !element.classList.contains("correctClass")) {
+            element.style = "fill: " + RGBcurrentColor;
+        }
+    }
+}
+
+// Function that reveals the selected bone segment on the image and adds the associated classes. * Note - The segments are already coloured, we just need to reveal them.
+function funcRevealFill(index) {
     var element = document.getElementById(arraySectionIDs[index]);
-    element.style.fill = currentColor;
     var selectedItems = document.getElementsByClassName("selectedClass");
 
-
+    // Applies the appropriate classes to each bone segment as it is clicked on - or removes it if you're clicking it again
     if (element.classList.contains("selectedClass")) {
         element.classList.remove("selectedClass");
     } else {
-
-
         for (var i = 0; i < selectedItems.length; i++) {
-            if (selectedItems[i].style.fill == currentColor) {
+            if (selectedItems[i].style.fill == RGBcurrentColor) {
                 selectedItems[i].classList.remove("selectedClass");
             }
         }
         element.classList.add("selectedClass");
     }
-
-
 }
 
+// Function to check the progress of what bone segments you've gotten correct and what you have gotten wrong. This function will also reset any of the incorrect bones segments, but leave the correct ones as they are. It also adds a 'Correct' class to the segment, indicating that it can't be reset or even changed to a different colour.
+function funcCheckProgress() {
 
+    //This line is essentially the same as GetElementID, but works slightly differently. <bit hazy on the details>
+    var colouredInSegments = document.querySelectorAll(".selectedClass");
 
+    //We go through each segment that has been coloured in (no need to check the ones that have not been)
+    for (var i = 0; i < colouredInSegments.length; i++) {
 
-document.getElementById("checker").addEventListener("click", check);
+        // Get the id of what has been coloured
+        var clickedID = colouredInSegments[i].id;
 
-function check() {
-    console.log("doing this once only!");
-    var colouredIn = document.querySelectorAll("selectedClass");
-    console.log(colouredIn);
-    var bob = colouredIn.length;
-    for (var i = 0; i < bob; i++) {
+        //If it ISN'T a grey section (ie, not a valid part), then we check to see if it is a correct element
+        if (clickedID != "section_21" && clickedID != "section_22" && clickedID != "section_23" && clickedID != "section_24" && clickedID != "section_25" && clickedID != "section_26") {
 
+            // Looks inside of arrayBoneParts, finds the `correct` value of the selected bone
+            var test = (arrayBoneParts.find(element => element.correct == clickedID));
 
-        //console.log(i);
-        console.log(bob + " coloured in length");
-
-        // get the id of what has been coloured
-        var clicked = colouredIn[i].id;
-
-
-
-        if (clicked != "section_21" && clicked != "section_22" && clicked != "section_23" && clicked != "section_24" && clicked != "section_25" && clicked != "section_26") {
-
-
-            //look inside of parts, find the `correct` value of the selected bone
-            var test = (parts.find(element => element.correct == clicked));
-
-            if (colouredIn[i].style.fill == test.color) {
-                //add a class of 'correctClass' to the element classList
-                colouredIn[i].classList.add("correctClass");
-                colouredIn[i].classList.remove("selectedClass");
-            } else {
-
-            }
+            // If it is a correct selection, we assign the 'CorrectClass' to it, meaning it can't ever be unselected
+            if (colouredInSegments[i].style.fill == test.color) {
+                colouredInSegments[i].classList.add("correctClass");
+                colouredInSegments[i].classList.remove("selectedClass");
+            } 
         }
-        colouredIn[i].classList.remove("selectedClass");
+        colouredInSegments[i].classList.remove("selectedClass");
     }
 }
